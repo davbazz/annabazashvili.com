3058609fe3528801006d8ac442f7b12c877ad42f
David <callmelmao@gmail.com>
2024-01-27 15:14:49 +0000

    reponsive mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <main className="">
       <Hero />
-      <About />
+      {/* <About />
       <Services />
       <Testimonials />
-      <FAQ />
+      <FAQ /> */}
     </main>
   )
 }
--- components/home/hero/component.tsx
+++ components/home/hero/component.tsx
@@ -14,7 +14,7 @@ export default function Hero() {
         <div className="relative flex w-full items-start justify-between">
           <div className="flex w-[60%] flex-col gap-12 text-nowrap xl:gap-16">
             <h1
-              className={`${montserrat.className} inline-block w-[400px] text-[22px] font-[700] leading-[34px] sm:w-auto sm:text-3xl sm:leading-[40px] xl:text-4xl xl:leading-[48px]`}
+              className={`${montserrat.className} inline-block w-[400px] text-[21px] font-[700] leading-[34px] sm:w-auto sm:text-3xl sm:leading-[40px] xl:text-4xl xl:leading-[48px]`}
             >
               <span className="italic">Guiding</span> Minds,{' '}
               <span className="italic">Inspiring</span> Hope:
@@ -23,10 +23,12 @@ export default function Hero() {
               <br />
               Begins <span className="italic">Here</span>.
             </h1>
-            <h3 className="inline-block w-[210px] text-wrap text-sm font-[500] italic leading-6 sm:w-[300px] md:w-[360px] lg:w-[420px] xl:w-[480px] xl:text-base xl:leading-7">
-              Discover a path to greater well-being with my professional
-              counseling services. Your mental health matters and I'am here to
-              help you thrive.
+            <h3 className="inline-block w-auto text-wrap text-sm font-[500] italic leading-6 sm:w-[300px] md:w-[360px] lg:w-[420px] xl:w-[480px] xl:text-base xl:leading-7">
+              Discover a path to <br className="sm:hidden" /> greater well-being{' '}
+              <br className="sm:hidden" />
+              with my professional <br className="sm:hidden" /> counseling
+              services. Your <br className="sm:hidden" /> mental health matters
+              and <br className="sm:hidden" /> I'am here to help you thrive.
             </h3>
             <div className="mt-8 flex w-[320px] gap-4 text-nowrap sm:w-auto md:mt-12 xl:mt-20">
               <ContactButton>Contact me</ContactButton>
@@ -34,7 +36,7 @@ export default function Hero() {
             </div>
           </div>
 
-          <div className="relative -ml-7 -mr-0 mt-14 flex h-[150%] w-[150%] items-center justify-center sm:-ml-4 md:-ml-6 md:mt-12 md:h-[300px] md:w-[300px] lg:-mr-10 lg:-mt-12 lg:ml-0 lg:h-[460px] lg:w-[460px] xl:-mt-20 xl:h-[700px] xl:w-[700px]">
+          <div className="relative -ml-12 -mr-0 mt-14 flex h-[150%] w-[150%] items-center justify-center sm:-ml-4 md:-ml-6 md:mt-12 md:h-[300px] md:w-[300px] lg:-mr-10 lg:-mt-12 lg:ml-0 lg:h-[460px] lg:w-[460px] xl:-mt-20 xl:h-[700px] xl:w-[700px]">
             <Blob />
             <iframe
               className="z-10 -ml-4 mt-[70px] h-[170px] sm:mt-20 md:ml-0 md:mt-8 lg:mt-0 lg:h-[240px] xl:-mt-20 xl:h-[330px]"
